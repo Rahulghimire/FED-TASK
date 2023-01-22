aa3fc6582737e92fae40f3964759db64a9c7ad4b
raul <ghimirerahul554@gmail.com>
2023-01-22 18:16:35 +0545

    icon-update

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -1,13 +1,14 @@
 import 'popper.js';
 // import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/dropdown';
 // import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/tab';
 // import 'bootstrap/js/dist/collapse';
 // import 'bootstrap-select';
 // import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/carousel';
 
 class global {
     constructor() {
